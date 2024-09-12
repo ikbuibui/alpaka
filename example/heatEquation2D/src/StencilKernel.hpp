d155cfa96e2544a338df017d7df2d323005c73a7
Tapish <narwal83@hzdr.de>
2024-09-12 17:11:32 +0200

    rearrange

--- example/heatEquation2D/src/StencilKernel.hpp
+++ example/heatEquation2D/src/StencilKernel.hpp
@@ -32,19 +32,19 @@ struct StencilKernel
         double const dy,
         double const dt) const -> void
     {
-        // Get extents(dimensions)
-        auto const blockThreadExtent = alpaka::getWorkDiv<alpaka::Block, alpaka::Threads>(acc);
-        auto const numThreadsPerBlock = blockThreadExtent.prod();
-
         // Get indexes
         auto const gridBlockIdx = alpaka::getIdx<alpaka::Grid, alpaka::Blocks>(acc);
         auto const blockThreadIdx = alpaka::getIdx<alpaka::Block, alpaka::Threads>(acc);
-        auto const threadIdx1D = alpaka::mapIdx<1>(blockThreadIdx, blockThreadExtent)[0u];
-        auto const blockStartIdx = gridBlockIdx * chunkSize;
+        auto const blockStartThreadIdx = gridBlockIdx * chunkSize;
 
         // Each kernel executes one element
         double const rX = dt / (dx * dx);
         double const rY = dt / (dy * dy);
+
+        auto const blockThreadExtent = alpaka::getWorkDiv<alpaka::Block, alpaka::Threads>(acc);
+        auto const numThreadsPerBlock = blockThreadExtent.prod();
+
+        auto const threadIdx1D = alpaka::mapIdx<1>(blockThreadIdx, blockThreadExtent)[0u];
 
         // go over only core cells
         for(auto i = threadIdx1D; i < chunkSize.prod(); i += numThreadsPerBlock)
@@ -52,7 +52,7 @@ struct StencilKernel
             auto idx2D = alpaka::mapIdx<2>(alpaka::Vec<alpaka::DimInt<1u>, TIdx>(i), chunkSize);
             idx2D = idx2D + alpaka::Vec<TDim, TIdx>{1, 1}; // offset for halo above and to the left
 
-            auto const bufIdx = idx2D + blockStartIdx;
+            auto const bufIdx = idx2D + blockStartThreadIdx;
 
             uNextBuf(bufIdx[0], bufIdx[1])
                 = uCurrBuf(bufIdx[0], bufIdx[1]) * (1.0 - 2.0 * rX - 2.0 * rY)
